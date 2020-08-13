39d4eeedb5de366082b953177cc6ffdc926bd90d
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:53:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ min = 0
+ newHash = nil
+ name_hash.each do |name, value|
+  if min == 0 || value < min
+    min = value
+    newHash = namelearn
+  end
+ end
+ newHash
 end
